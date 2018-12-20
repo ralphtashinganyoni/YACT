da81bf82c69d55f3695c21c42c27fe33b6c0933a
Marlon Miles MG <Marlon Miles MG@DESKTOP-B06C2L9>
2018-12-20 17:47:02 +0200

    Spaces under each footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationCancel, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+/*   loading = true;
+  constructor(private _router: Router) {
+    this._router.events.subscribe((routerEvent: Event) => {
+      if (routerEvent instanceof NavigationStart) {
+        this.loading = true;
+      }
+      if (routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationError) {
+        this.loading = false;
+      }
+
+    });
+  } */
   title = 'app';
 }
